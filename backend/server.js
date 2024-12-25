8b383d052cc9e6d98ded18a3a70bd6dfec5d67b0
snaxy318 <suryanshtejas@gmail.com>
2024-12-25 17:40:21 +0530

    added routes

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,9 @@
 import express from 'express';
+import authRoutes from './routes/auth.route.js';
 
 const app = express();
 
-app.get("/",(req,res)=> {
-    res.send("server is ready.");
-})
+app.use("/api/v1/auth",authRoutes);
 
 app.listen(5000,() => {
     console.log('Server started at http://localhost:5000');
