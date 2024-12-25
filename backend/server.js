fb5e3fda5c674f55c014de9fdecfa9392ad4519e
snaxy318 <suryanshtejas@gmail.com>
2024-12-25 23:28:45 +0530

    allowed to parse req.body

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,8 @@ import { connectDB } from './config/db.js';
 const app = express();
 const PORT = ENV_VARS.PORT
 
+app.use(express.json()); //will allow us to parse req.body
+
 app.use("/api/v1/auth",authRoutes);
 
 app.listen(PORT,() => {
