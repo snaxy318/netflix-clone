db0439333b8bc1c488a0c0695b0d3b477e5bf9cd
snaxy318 <suryanshtejas@gmail.com>
2024-12-28 11:24:47 +0530

    added NODE_ENV to development

--- backend/config/envVars.js
+++ backend/config/envVars.js
@@ -5,5 +5,6 @@ dotenv.config();
 export const ENV_VARS = {
     MONGO_URL:process.env.MONGO_URL,
     PORT: process.env.PORT || 5000,
-    JWT_SECRET: process.env.JWT_SECRET
+    JWT_SECRET: process.env.JWT_SECRET,
+    NODE_ENV: process.env.NODE_ENV
 }
