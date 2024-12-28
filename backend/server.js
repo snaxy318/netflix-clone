3a82b3022c63b94e2451dfa8e32d1f469f31e066
snaxy318 <suryanshtejas@gmail.com>
2024-12-28 16:34:02 +0530

    added routing for movie API

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,7 @@ const PORT = ENV_VARS.PORT
 app.use(express.json()); //will allow us to parse req.body
 
 app.use("/api/v1/auth",authRoutes);
+app.use("api/v1/movie",movieRoutes);
 
 app.listen(PORT,() => {
     console.log('Server started at http://localhost:'+PORT);
