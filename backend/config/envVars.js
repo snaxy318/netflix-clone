b1b6cb2880ceebb03f482cf81c13823ad7e4fb14
snaxy318 <suryanshtejas@gmail.com>
2024-12-28 16:18:43 +0530

    added TMDB API key

--- backend/config/envVars.js
+++ backend/config/envVars.js
@@ -6,5 +6,6 @@ export const ENV_VARS = {
     MONGO_URL:process.env.MONGO_URL,
     PORT: process.env.PORT || 5000,
     JWT_SECRET: process.env.JWT_SECRET,
-    NODE_ENV: process.env.NODE_ENV
+    NODE_ENV: process.env.NODE_ENV,
+    TMDB_KEY: process.env.TMDB_KEY
 }
