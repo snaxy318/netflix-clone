d2b4bd114e3a6dc99096bf61c4c720e9e0941687
snaxy318 <suryanshtejas@gmail.com>
2024-12-29 11:47:08 +0530

    added route for getting movie trailer

--- backend/routes/movie.route.js
+++ backend/routes/movie.route.js
@@ -4,5 +4,6 @@ import { getTrendingMovie } from "../controller/movie.controller.js";
 const router = express.Router();
 
 router.get('/trending',getTrendingMovie);
+router.get('/:id/trailer',getMovieTrailer);
 
 export default router;
