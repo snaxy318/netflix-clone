a1fcfb15d2f4183f47cd5b715c7e0341bbd71a4d
snaxy318 <suryanshtejas@gmail.com>
2024-12-30 21:54:51 +0530

    added route and controller for similar tv shows

--- backend/routes/tv.route.js
+++ backend/routes/tv.route.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { getTrendingTvShow, getTvShowDetails, getTvShowTrailer } from "../controller/tv.controller.js";
+import { getSimilarTvShows, getTrendingTvShow, getTvShowDetails, getTvShowTrailer } from "../controller/tv.controller.js";
 
 const router = express.Router();
 
 router.get('/trending',getTrendingTvShow);
 router.get('/:id/trailer',getTvShowTrailer);
 router.get('/:id/details',getTvShowDetails);
+router.get('/:id/similar',getSimilarTvShows);
 
 export default router;
