b2d7285b5f70989db5ee2b2aaad2724393f2fc02
snaxy318 <suryanshtejas@gmail.com>
2024-12-30 14:31:36 +0530

    added tv route

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import authRoutes from './routes/auth.route.js';
-import movieRoutes from './routes/movie.route.js'
+import movieRoutes from './routes/movie.route.js';
+import tvRoutes from './routes/tv.route.js';
 import { ENV_VARS } from './config/envVars.js';
 import { connectDB } from './config/db.js';
 
@@ -11,6 +12,7 @@ app.use(express.json()); //will allow us to parse req.body
 
 app.use("/api/v1/auth",authRoutes);
 app.use("/api/v1/movie",movieRoutes);
+app.use("/api/v1/tv",tvRoutes);
 
 app.listen(PORT,() => {
     console.log('Server started at http://localhost:'+PORT);
