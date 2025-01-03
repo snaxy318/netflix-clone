bc9bec7e05f83f7bd231cd20280adace15106200
snaxy318 <suryanshtejas@gmail.com>
2025-01-03 18:15:24 +0530

    added getSearchHistory

--- backend/controller/search.controller.js
+++ backend/controller/search.controller.js
@@ -84,3 +84,11 @@ export async function searchTv(req,res) {
         res.status(500).json({success:false,message:"Internal Server error"});
     }
 }
+
+export async function getSearchHistory(req,res) {
+    try {
+        res.status(200).json({success:true,content:req.user.searchHistory});
+    } catch (error) {
+        res.status(500).json({success:false,message:"Internal server error"});
+    }   
+}
--- backend/routes/search.route.js
+++ backend/routes/search.route.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { searchMovie, searchPerson, searchTv } from "../controller/search.controller.js";
+import { getSearchHistory, searchMovie, searchPerson, searchTv } from "../controller/search.controller.js";
 
 const router = express.Router();
 
 router.get("/person/:query",searchPerson);
 router.get("/movie/:query",searchMovie);
 router .get("/tv/:query",searchTv);
+
+router.get("/history",getSearchHistory);
 
 export default router;
