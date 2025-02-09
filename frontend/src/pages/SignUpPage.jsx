9c36c73bd81e88ff720c610d7b27d5d965c50ac2
snaxy318 <suryanshtejas@gmail.com>
2025-02-09 17:07:52 +0530

    added netflix logo to header

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,7 +1,15 @@
+import {Link} from "react-router-dom"
+
 const SignUpPage = () => {
-  return (
-    <div>SignUpPage</div>
-  )
+  return <div className='h-screen w-full hero-bg'>
+    <header className="max-w-6xl mx-auto flex items-center justify-between p-4">
+
+        <Link to={"/"}>
+            <img src='/netflix-logo.png' alt='logo' className="w-52"/>
+        </Link>
+
+    </header>
+  </div>
 }
 
 export default SignUpPage
