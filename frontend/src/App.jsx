8a80cdc4a3e41afac4bb1b4ab56912c2d272a14d
snaxy318 <suryanshtejas@gmail.com>
2025-02-09 15:49:35 +0530

    added HomePage,LoginPage and SignUpPage router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,16 @@
+import {Route,Routes} from 'react-router-dom';
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
+
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline text-red-600">
-      Hello world!
-    </h1>
-  )
+    <Routes>
+      <Route path='/' element={<HomePage/>}/>
+      <Route path='/signup' element={<SignUpPage/>}/>
+      <Route path='/login' element={<LoginPage/>}/>
+    </Routes>
+  ); 
 }
 
 export default App
